3cf85a21e2f8a694dbffc74104633dfae1777401
Daniela <daniela.echezuria.cel@gmail.com>
2020-07-31 11:38:12 -0400

    Modificación de interfaz con validaciones de campos

--- src/Interfaz/MenuInicial.java
+++ src/Interfaz/MenuInicial.java
@@ -12,20 +12,17 @@ import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.Font;
 import javax.swing.JTextField;
-import java.awt.GridLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JPasswordField;
 
 public class MenuInicial extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField textUser;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -44,40 +41,76 @@ public class MenuInicial extends JDialog {
 	 * Create the dialog.
 	 */
 	public MenuInicial() {
+		setResizable(false);
 		setBounds(100, 100, 349, 246);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBackground(new Color(135, 206, 250));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
 		
 		JLabel label = new JLabel("");
+		label.setBounds(97, 8, 66, 66);
 		label.setIcon(new ImageIcon(MenuInicial.class.getResource("/Imagenes/esfera2048.png")));
-		contentPanel.add(label, "cell 0 0,grow");
+		contentPanel.add(label);
 		
 		JLabel label_1 = new JLabel("");
-		contentPanel.add(label_1, "cell 1 0,grow");
+		label_1.setBounds(100, 43, 0, 0);
+		contentPanel.add(label_1);
 		
-		JLabel lblNombreDeUsuario = new JLabel("NOMBRE DE USUARIO");
-		lblNombreDeUsuario.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		contentPanel.add(lblNombreDeUsuario, "cell 0 1,grow");
+		JLabel lblNombreDeUsuario = new JLabel("USUARIO:");
+		lblNombreDeUsuario.setBounds(97, 88, 66, 14);
+		lblNombreDeUsuario.setFont(new Font("Georgia", Font.ITALIC, 12));
+		contentPanel.add(lblNombreDeUsuario);
 		
-		textField_1 = new JTextField();
-		contentPanel.add(textField_1, "cell 1 1,grow");
-		textField_1.setColumns(10);
+		textUser = new JTextField();
+		textUser.setBounds(175, 85, 86, 20);
+		contentPanel.add(textUser);
+		textUser.setColumns(10);
 		
-		JLabel lblContrasea = new JLabel("CONTRASE\u00D1A");
-		lblContrasea.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		contentPanel.add(lblContrasea, "cell 0 2,grow");
-		
-		textField = new JTextField();
-		contentPanel.add(textField, "cell 1 2,grow");
-		textField.setColumns(10);
+		JLabel lblContrasea = new JLabel("CONTRASE\u00D1A:");
+		lblContrasea.setBounds(70, 122, 93, 14);
+		lblContrasea.setFont(new Font("Georgia", Font.ITALIC, 12));
+		contentPanel.add(lblContrasea);
 		
 		JLabel label_3 = new JLabel("");
-		contentPanel.add(label_3, "cell 1 3,grow");
+		label_3.setBounds(246, 91, 0, 0);
+		contentPanel.add(label_3);
 		
 		JLabel label_4 = new JLabel("");
-		contentPanel.add(label_4, "cell 0 4,grow");
+		label_4.setBounds(251, 91, 0, 0);
+		contentPanel.add(label_4);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBackground(Color.WHITE);
+		passwordField.setBounds(175, 119, 86, 20);
+		contentPanel.add(passwordField);
+		
+		JLabel lblbtree = new JLabel("-BTree");
+		lblbtree.setForeground(new Color(0, 51, 204));
+		lblbtree.setFont(new Font("Georgia", Font.BOLD, 29));
+		lblbtree.setBounds(163, 24, 123, 38);
+		contentPanel.add(lblbtree);
+		{
+			JButton btnIngresar = new JButton("Ingresar");
+			btnIngresar.setBounds(127, 151, 93, 23);
+			contentPanel.add(btnIngresar);
+			btnIngresar.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					//busca username y pass y compara
+					
+						Tablero frame= new Tablero();
+						frame.setVisible(true);
+						setVisible(false);
+						dispose();
+			
+					
+				}
+			});
+			btnIngresar.setActionCommand("OK");
+			getRootPane().setDefaultButton(btnIngresar);
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -95,28 +128,9 @@ public class MenuInicial extends JDialog {
 			btnRegistrarUsuario.setActionCommand("OK");
 			buttonPane.add(btnRegistrarUsuario);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseClicked(MouseEvent e) {
-						//busca username y pass y compara
-						
-							Tablero frame= new Tablero();
-							frame.setVisible(true);
-							setVisible(false);
-							dispose();
-				
-						
-					}
-				});
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				JButton btnSalir = new JButton("Salir");
+				btnSalir.setActionCommand("Cancel");
+				buttonPane.add(btnSalir);
 			}
 		}
 	}
--- src/Interfaz/RegistroUsuario.java
+++ src/Interfaz/RegistroUsuario.java
@@ -1,9 +1,10 @@
 package Interfaz;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -14,16 +15,21 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JPasswordField;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class RegistroUsuario extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField textNombre;
+	private JTextField textApellido;
+	private JTextField textUsername;
+	private JTextField textEmail;
+	private JPasswordField passwordFieldConfirm;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -37,23 +43,28 @@ public class RegistroUsuario extends JDialog {
 			e.printStackTrace();
 		}
 	}
-
 	
-	public int toInt(String a){
-		int b;
-		try {
-		   b = Integer.parseInt(a);
-		}
-		catch (NumberFormatException e)
-		{
-		   b = 0;
-		}
-		return b;
+	private void verificarTecla(KeyEvent e){
+		char caracter = e.getKeyChar();
+		if ((!Character.isLetter(caracter))&&(!Character.isSpace(caracter)))
+			e.consume();
+			//finaliza este evento para que la fuente que lo origino no lo procese de la manera determinada como si no hubiera presionado la tecla
 	}
+	
+	private boolean verificarEmail(String email){
+		// Patrón para validar el email
+		Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+		Matcher mather = pattern.matcher(email);
+		if (mather.find() == true) return true;
+		else return false;
+	}
+	
 	/**
 	 * Create the dialog.
 	 */
 	public RegistroUsuario() {
+		setResizable(false);
+		getContentPane().setBackground(new Color(173, 216, 230));
 		JLabel lblNewLabel = new JLabel("");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(null);
@@ -62,42 +73,210 @@ public class RegistroUsuario extends JDialog {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel);
 		{
+			JLabel lblRegistroDeUsuario = new JLabel("REGISTRO DE JUGADOR");
+			lblRegistroDeUsuario.setForeground(new Color(0, 0, 153));
+			lblRegistroDeUsuario.setBackground(new Color(0, 0, 139));
+			lblRegistroDeUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+			lblRegistroDeUsuario.setFont(new Font("Georgia", Font.BOLD, 15));
+			lblRegistroDeUsuario.setBounds(10, 11, 414, 14);
+			getContentPane().add(lblRegistroDeUsuario);
+			
+			JLabel lblApellido = new JLabel("Apellido:");
+			lblApellido.setFont(new Font("Georgia", Font.PLAIN, 12));
+			lblApellido.setBounds(84, 75, 51, 14);
+			getContentPane().add(lblApellido);
+			
+			JLabel lblNombre = new JLabel("Nombre:");
+			lblNombre.setFont(new Font("Georgia", Font.PLAIN, 12));
+			lblNombre.setBounds(84, 45, 51, 14);
+			getContentPane().add(lblNombre);
+			
+			JLabel lblUsername = new JLabel("Username:");
+			lblUsername.setFont(new Font("Georgia", Font.PLAIN, 12));
+			lblUsername.setBounds(72, 142, 63, 14);
+			getContentPane().add(lblUsername);
+			
+			JLabel lblEmail = new JLabel("E-mail:");
+			lblEmail.setFont(new Font("Georgia", Font.PLAIN, 12));
+			lblEmail.setBounds(94, 109, 63, 14);
+			getContentPane().add(lblEmail);
+			
+			JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
+			lblContrasea.setFont(new Font("Georgia", Font.PLAIN, 12));
+			lblContrasea.setBounds(104, 178, 80, 14);
+			getContentPane().add(lblContrasea);
+			
+			JLabel lblConfirmarContrasea = new JLabel("Confirmar contrase\u00F1a:");
+			lblConfirmarContrasea.setFont(new Font("Georgia", Font.PLAIN, 12));
+			lblConfirmarContrasea.setBounds(54, 203, 144, 14);
+			getContentPane().add(lblConfirmarContrasea);
+			
+			textNombre = new JTextField();
+			textNombre.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyTyped(KeyEvent e) {
+					verificarTecla(e);				
+				}
+			});
+			textNombre.setBounds(139, 42, 250, 20);
+			getContentPane().add(textNombre);
+			textNombre.setColumns(10);
+			
+			textApellido = new JTextField();
+			textApellido.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyTyped(KeyEvent e) {
+					verificarTecla(e);				
+				}
+			});
+			textApellido.setColumns(10);
+			textApellido.setBounds(139, 73, 250, 20);
+			getContentPane().add(textApellido);
+			
+			textUsername = new JTextField();
+			textUsername.setColumns(10);
+			textUsername.setBounds(139, 139, 250, 20);
+			getContentPane().add(textUsername);
+			
+			textEmail = new JTextField();
+			textEmail.setColumns(10);
+			textEmail.setBounds(139, 106, 250, 20);
+			getContentPane().add(textEmail);
+			
+			passwordFieldConfirm = new JPasswordField();
+			passwordFieldConfirm.setBounds(195, 200, 194, 20);
+			getContentPane().add(passwordFieldConfirm);
+			
+			passwordField = new JPasswordField();
+			passwordField.setBounds(194, 175, 194, 20);
+			getContentPane().add(passwordField);
+			
+			JLabel lblErrorNombre = new JLabel("*Nombre invalido");
+			lblErrorNombre.setHorizontalAlignment(SwingConstants.RIGHT);
+			lblErrorNombre.setFont(new Font("Georgia", Font.PLAIN, 11));
+			lblErrorNombre.setForeground(new Color(255, 0, 0));
+			lblErrorNombre.setBounds(54, 30, 335, 14);
+			lblErrorNombre.setVisible(false);
+			getContentPane().add(lblErrorNombre);
+			
+			JLabel lblErrorContraseñas = new JLabel("*Contrase\u00F1as invalidas");
+			lblErrorContraseñas.setHorizontalAlignment(SwingConstants.RIGHT);
+			lblErrorContraseñas.setFont(new Font("Georgia", Font.PLAIN, 11));
+			lblErrorContraseñas.setForeground(Color.RED);
+			lblErrorContraseñas.setBounds(54, 151, 334, 38);
+			lblErrorContraseñas.setVisible(false);
+			getContentPane().add(lblErrorContraseñas);
+			
+			JLabel lblErrorUser = new JLabel("*User invalido");
+			lblErrorUser.setHorizontalAlignment(SwingConstants.RIGHT);
+			lblErrorUser.setFont(new Font("Georgia", Font.PLAIN, 11));
+			lblErrorUser.setForeground(Color.RED);
+			lblErrorUser.setBounds(54, 126, 335, 14);
+			lblErrorUser.setVisible(false);
+			getContentPane().add(lblErrorUser);
+			
+			JLabel lblErrorEmail = new JLabel("*Email invalido");
+			lblErrorEmail.setHorizontalAlignment(SwingConstants.RIGHT);
+			lblErrorEmail.setFont(new Font("Georgia", Font.PLAIN, 11));
+			lblErrorEmail.setForeground(Color.RED);
+			lblErrorEmail.setBounds(54, 94, 335, 14);
+			lblErrorEmail.setVisible(false);
+			getContentPane().add(lblErrorEmail);
+			
+			JLabel lblErrorApellido = new JLabel("*Apellido invalido");
+			lblErrorApellido.setHorizontalAlignment(SwingConstants.RIGHT);
+			lblErrorApellido.setFont(new Font("Georgia", Font.PLAIN, 11));
+			lblErrorApellido.setForeground(Color.RED);
+			lblErrorApellido.setBounds(54, 61, 335, 14);
+			lblErrorApellido.setVisible(false);
+			getContentPane().add(lblErrorApellido);
+			
+			
 			JPanel buttonPane = new JPanel();
-			buttonPane.setBounds(0, 228, 434, 33);
+			buttonPane.setBounds(0, 228, 444, 44);
 			getContentPane().add(buttonPane);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setBounds(265, 5, 70, 23);
-				okButton.addMouseListener(new MouseAdapter() {
+				JButton btnRegistrar = new JButton("Registrar");
+				btnRegistrar.setBounds(243, 9, 89, 23);
+				btnRegistrar.addMouseListener(new MouseAdapter() {
+					@SuppressWarnings("deprecation")
 					@Override
 					public void mouseClicked(MouseEvent e) {
-						int contrasena=toInt(textField_4.getText()), contracheck=toInt(textField_5.getText());
-						//verificar que todas las condiciones se cumplan:
-						if (contrasena!=contracheck){
-							lblNewLabel.setText("Las contraseñas no coinciden.");
-							return;
+						lblErrorNombre.setVisible(false);
+						lblErrorApellido.setVisible(false);
+						lblErrorUser.setVisible(false);
+						lblErrorEmail.setVisible(false);
+						lblErrorContraseñas.setVisible(false);
+						boolean error=false;
+						if (textNombre.getText().length() < 3){
+							lblErrorNombre.setText("Error: nombre NO puede ser menor a 3 caracteres");
+							lblErrorNombre.setVisible(true);
+							error=true;
+						}
+						if (textNombre.getText().isEmpty()){
+							lblErrorNombre.setText("Error: nombre NO puede estar en blanco");
+							lblErrorNombre.setVisible(true);
+							error=true;
+						}
+						if (textApellido.getText().length() < 4){
+							lblErrorApellido.setText("Error: apellido NO puede ser menor a 4 caracteres");
+							lblErrorApellido.setVisible(true);
+							error=true;
+						}
+						if (textApellido.getText().isEmpty()){
+							lblErrorApellido.setText("Error: apellido NO puede estar en blanco");
+							lblErrorApellido.setVisible(true);
+							error=true;
+						}
+						if (textEmail.getText().isEmpty()){
+							lblErrorUser.setText("Error: email NO puede estar en blanco");
+							lblErrorUser.setVisible(true);
+							error=true;
+						}
+						if (!verificarEmail(textEmail.getText())){
+							lblErrorEmail.setText("Error: email invalido");
+							lblErrorEmail.setVisible(true);
+							error=true;
+						}
+						if (textEmail.getText().isEmpty()){
+							lblErrorEmail.setText("Error: email NO puede estar en blanco");
+							lblErrorEmail.setVisible(true);
+							error=true;
+						}
+						if (passwordField.getText().isEmpty()||passwordFieldConfirm.getText().isEmpty()){
+							lblErrorContraseñas.setText("Error: contraseña en blanco");
+							lblErrorContraseñas.setVisible(true);
+							error=true;
+						}
+						if (String.valueOf(passwordField.getText()).equals(String.valueOf(passwordFieldConfirm.getText())));
+						else{
+							lblErrorContraseñas.setText("Error: contraseñas no coinciden.");
+							lblErrorContraseñas.setVisible(true);
+							error=true;
+							
+						}
+							
 						
+						if (!error){
+							setVisible(false); //you can't see me!
+							MenuInicial a = new MenuInicial();
+							a.setVisible(true);
+							dispose(); //Destroy the JFrame object
+						}
 					}
-						//...
-						//despues de todos los checks
-						setVisible(false); //you can't see me!
-						MenuInicial a = new MenuInicial();
-						a.setVisible(true);
-						dispose(); //Destroy the JFrame object
-						
-				}});
+				});
 				buttonPane.setLayout(null);
 				
 				
 				lblNewLabel.setBounds(10, 9, 248, 14);
 				buttonPane.add(lblNewLabel);
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				btnRegistrar.setActionCommand("OK");
+				buttonPane.add(btnRegistrar);
+				getRootPane().setDefaultButton(btnRegistrar);
 			}
 			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.addMouseListener(new MouseAdapter() {
+				JButton btnCancelar = new JButton("Cancelar");
+				btnCancelar.addMouseListener(new MouseAdapter() {
 					@Override
 					public void mouseClicked(MouseEvent e) {
 						setVisible(false); //you can't see me!
@@ -106,84 +285,11 @@ public class RegistroUsuario extends JDialog {
 						dispose(); //Destroy the JFrame object
 					}
 				});
-				cancelButton.setBounds(340, 5, 89, 23);
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				btnCancelar.setBounds(345, 9, 89, 23);
+				btnCancelar.setActionCommand("Cancel");
+				buttonPane.add(btnCancelar);
 			}
-		}
-		
-		JLabel lblRegistroDeUsuario = new JLabel("REGISTRO DE USUARIO");
-		lblRegistroDeUsuario.setHorizontalAlignment(SwingConstants.CENTER);
-		lblRegistroDeUsuario.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-		lblRegistroDeUsuario.setBounds(10, 11, 414, 14);
-		getContentPane().add(lblRegistroDeUsuario);
-		
-		JLabel lblApellido = new JLabel("Apellido");
-		lblApellido.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		lblApellido.setBounds(44, 76, 46, 14);
-		getContentPane().add(lblApellido);
-		
-		JLabel lblNombre = new JLabel("Nombre");
-		lblNombre.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		lblNombre.setBounds(44, 51, 46, 14);
-		getContentPane().add(lblNombre);
-		
-		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		lblUsername.setBounds(44, 101, 63, 14);
-		getContentPane().add(lblUsername);
-		
-		JLabel lblEmail = new JLabel("E-mail");
-		lblEmail.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		lblEmail.setBounds(44, 126, 63, 14);
-		getContentPane().add(lblEmail);
-		
-		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
-		lblContrasea.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		lblContrasea.setBounds(44, 151, 63, 14);
-		getContentPane().add(lblContrasea);
-		
-		JLabel lblConfirmarContrasea = new JLabel("Confirmar contrase\u00F1a");
-		lblConfirmarContrasea.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		lblConfirmarContrasea.setBounds(44, 176, 110, 14);
-		getContentPane().add(lblConfirmarContrasea);
-		
-		textField = new JTextField();
-		textField.setBounds(100, 48, 296, 20);
-		getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(100, 73, 296, 20);
-		getContentPane().add(textField_1);
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(100, 98, 296, 20);
-		getContentPane().add(textField_2);
-		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(100, 123, 296, 20);
-		getContentPane().add(textField_3);
-		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(110, 148, 286, 20);
-		getContentPane().add(textField_4);
-		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(152, 173, 244, 20);
-		getContentPane().add(textField_5);
-
-		
-		
+		}	
 		
 	}
-	
-	
-
-
 }
