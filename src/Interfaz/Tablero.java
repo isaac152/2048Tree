451d467f67e50a8bcf8549dab7906084c52a65d4
Daniela <daniela.echezuria.cel@gmail.com>
2020-08-13 07:10:54 -0400

    "Ramas dinámicas, falta acomodar"

--- src/Interfaz/Tablero.java
+++ src/Interfaz/Tablero.java
@@ -51,6 +51,12 @@ public class Tablero extends JFrame implements ActionListener{
 	Contenedor varas2=new Contenedor();
 	Contenedor varas3=new Contenedor();
 	Contenedor varas4=new Contenedor();
+	int level1=0,level2=1,level3=1,level4=1;
+	int gridx=-1;
+	Palito conector1;
+	Palito conector2;
+	Palito conector3;
+	Palito conector4,conector5;
 
 	/**
 	 * Launch the application.
@@ -169,42 +175,6 @@ public class Tablero extends JFrame implements ActionListener{
 		varas4.gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.add(varas4.panel);
 
-
-		//Conectores
-		
-		Palito conector1 = new Palito(1);
-		conector1.gbc_label.gridx=1;
-		varas1.panel.add(conector1.label,conector1.gbc_label);
-		
-		Palito conector2 = new Palito(2);
-		conector2.gbc_label.gridx = 3;
-		varas1.panel.add(conector2.label, conector2.gbc_label);
-		
-		int gridx=-1;
-		for (int i=1;i<5;i++){
-			Palito conector3= new Palito(i%2);
-			gridx=gridx+2;
-			conector3.gbc_label.gridx=gridx;
-			varas2.panel.add(conector3.label,conector3.gbc_label);
-		}
-		gridx=-1;
-		
-		for (int i=1;i<9;i++){
-			Palito conector4 =new Palito(i%2);
-			gridx=gridx+2;
-			conector4.gbc_label.gridx=gridx;
-			varas3.panel.add(conector4.label,conector4.gbc_label);
-		
-		}
-		gridx=-1;
-		
-		for (int i=1;i<17;i++){
-			Palito conector5 =new Palito(i%2);
-			gridx=gridx+2;
-			conector5.gbc_label.gridx=gridx;
-			varas4.panel.add(conector5.label,conector5.gbc_label);
-		}
-				
 		//Contenedores - Niveles
 		nivel0.panel.setBounds(6, 6, 1323, 75);
 		nivel0.gbl_panel.columnWidths = new int[]{629, 7, 0};
@@ -241,6 +211,10 @@ public class Tablero extends JFrame implements ActionListener{
 		btnReiniciarPartida.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Global.Reinicio();
+				varas1.panel.setVisible(false);
+				varas2.panel.setVisible(false);
+				varas3.panel.setVisible(false);
+				varas4.panel.setVisible(false);
 				validate();
 				System.out.println("reset");
 				
@@ -306,16 +280,59 @@ public class Tablero extends JFrame implements ActionListener{
     public void Pintarcontenedor(int nivel,Nodo graf){
     	switch(nivel){
     	case 1:
+    		varas1.panel.setVisible(true);
+    		if (level1 == 0){
+    			conector1 = new Palito(1);
+    			conector1.gbc_label.gridx=1;
+    			varas1.panel.add(conector1.label,conector1.gbc_label);
+    		}else{
+    			Palito conector2 = new Palito(2);
+    			conector2.gbc_label.gridx = 3;
+    			varas1.panel.add(conector2.label, conector2.gbc_label);
+    			level1=-1;
+    		}
+    		level1++;
+    		gridx=-1;
     		nivel1.panel.add(graf.labelNodo, graf.gbc_graf);
     		break;
     	case 2:
+    		varas2.panel.setVisible(true);
+    		conector3= new Palito(level2%2);
+    		gridx=gridx+2;
+    		conector3.gbc_label.gridx=gridx;
+   			varas2.panel.add(conector3.label,conector3.gbc_label);
+    		level2++;
+    		if (level2==5){
+    			level2=1;
+    			gridx=-1; 		
+    		}
     		nivel2.panel.add(graf.labelNodo, graf.gbc_graf);
     		break;
     	case 3:
+    		varas3.panel.setVisible(true);
+    		conector4 =new Palito(level3%2);
+    		gridx=gridx+2;
+    		conector4.gbc_label.gridx=gridx;
+   			varas3.panel.add(conector4.label,conector4.gbc_label); 
+   			level3++;
+    		if (level3==9){
+    			level3=1;
+    			gridx=-1; 
+    		}
     		nivel3.panel.add(graf.labelNodo, graf.gbc_graf);
     		break;
     	case 4:
+    		varas4.panel.setVisible(true);
+    		conector5 =new Palito(level4%2);
+    		gridx=gridx+2;
+    		conector5.gbc_label.gridx=gridx;
+    		varas4.panel.add(conector5.label,conector5.gbc_label);
     		nivel4.panel.add(graf.labelNodo, graf.gbc_graf);
+    		level4++;
+    		if (level4==17){
+    			level4=1;
+    			gridx=-1; 
+    		}
     		break;
     	}
     }
